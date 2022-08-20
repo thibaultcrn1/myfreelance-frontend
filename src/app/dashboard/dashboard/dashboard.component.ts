cd61648245afaea7d239b23b6b8086a7c5fb45a9
thibaultcrn1 <tcarron6@outlook.com>
2022-08-20 02:58:24 +0200

    Update dashboard page

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  date!: string;
+  best_client!: string;
+  articles!: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.date = moment().format("DD/MM/YYYY");
+    this.best_client = "John Doe";
+    this.articles = "0";
+
   }
 
 }
