4db8a4b2ae13270062c984d89cef69bd81695d2a
thibaultcrn1 <tcarron6@outlook.com>
2022-08-22 02:32:41 +0200

    Create dashboard design

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -9,17 +9,62 @@ import * as moment from 'moment';
 export class DashboardComponent implements OnInit {
 
   date!: string;
-  best_client!: string;
+  clients!: string;
   articles!: string;
+  factures!: string;
+  notva!: string;
+  tva!: string;
+  maxtva!: string;
+  chiffre!: string;
+  year!: string;
 
   constructor() { }
 
   ngOnInit(): void {
 
     this.date = moment().format("DD/MM/YYYY");
-    this.best_client = "John Doe";
+    this.clients = "0";
     this.articles = "0";
+    this.factures = "0";
+    this.notva = "100";
+    this.tva = "1500";
+    this.maxtva = "10000";
+    this.chiffre = "0";
+    this.year = moment().format('YYYY');
 
+  }
+
+  public data = [
+    {
+      x: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+      y: [14500, 23000, 50400, 3444, 2345, 23333, 55555, 17893, 37282, 29019, 29301, 29302],
+      type: 'bar'
+    }
+  ]
+
+  public layout = {
+    title: "Chiffre d'affaires",
+    showlegend: false,
+    width: 800,
+    height: 330,
+    xaxis: {
+      showticklabels: false,
+      autotick: false,
+      showgrid: false,
+      zeroline: false
+    },
+    yaxis: {
+      showticklabels: false,
+      autotick: false,
+      showgrid: false,
+      zeroline: false
+    },
+  }
+
+  public config = {
+    displayModeBar: false,
+    locale: 'fr',
+    responsive: true
   }
 
 }
