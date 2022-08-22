e546a30b57770cdc836f727321c994cdf67b9d7a
thibaultcrn1 <tcarron6@outlook.com>
2022-08-22 02:31:55 +0200

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     SidebarComponent,
-  ]
+  ],
+  providers: [CookieService]
 })
 export class SharedModule { }
