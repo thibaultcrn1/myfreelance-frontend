6586020803d09910f3f823103bc99a0f5a612c6d
thibaultcrn1 <tcarron6@outlook.com>
2022-08-22 02:31:11 +0200

    Uodate sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { userInfo, generateAccessToken } from 'src/utils/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  username!: string;
+
+  constructor(private cookie: CookieService, private router: Router) { }
 
   ngOnInit(): void {
+
+    //const connect = this.cookie.get('connect.sid');
+    //if(connect === null) return this.router.navigate(['/', 'login']);
+    
+    //const accessToken = this.cookie.get('MyFreelanceAccess');
+    //const refreshToken = this.cookie.get('MyFreelanceRefresh');
+
+    //if(refreshToken === null) return this.router.navigate(['/', 'login']);
+    //if(accessToken === null) generateAccessToken(refreshToken);
+
+    //userInfo(accessToken)
+    //.then((result) => {
+    //  return this.username = result.data[0].firstname
+    //})
+    //.catch((err) => {
+    //  return this.router.navigate(['/', 'login']);
+    //});
+
+    this.username = "Thibault";
+
   }
 
 }
