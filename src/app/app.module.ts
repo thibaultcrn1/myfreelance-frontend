51a30405411ee5a489751ccd05c2c9771bbf8d3f
thibaultcrn1 <tcarron6@outlook.com>
2022-11-04 17:15:27 +0100

    Import interceptors provider in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastService, AngularToastifyModule } from 'angular-toastify'; 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtAuthorizationInterceptorsProvider } from './interceptors/jwt-authorization-interceptors/jwt-authorization-interceptors.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -19,8 +21,13 @@ import { RouterModule } from '@angular/router';
     DashboardModule,
     RouterModule,
     AngularToastifyModule,
+    HttpClientModule
   ],
-  providers: [ToastService],
+  providers: [
+    ToastService, 
+    CookieService,
+    JwtAuthorizationInterceptorsProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
